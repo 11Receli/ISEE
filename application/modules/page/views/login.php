d2cf73ab8afdbb58a3b4307cea64e75fafab64ef
11Receli <clairerenosa@gmail.com>
2016-12-26 15:07:41 +0800

    registration form

--- application/modules/page/views/login.php
+++ application/modules/page/views/login.php
@@ -1,9 +1,29 @@
+<head>
+    {head}
+</head>
 <?php echo form_open(); ?>
-<div>
-	<input type="text" name="username">
-</div>
-<input type="password" name="password">
-<button type="submit" name="submit">Login</button>
-<p>New to this site? <a href="<?php echo base_url('page/registration'); ?>">Register here!</a></p>
+<body>
+	<div class="container">
+		<form class="form-signin">
+			<h2 class="form-signin-heading">Login Here</h2>
+			<label for="username" class="sr-only">Email address</label>
+			<input type="text" name="username" class="form-control" placeholder="User Name" required="" autofocus="">
+			<label for="password" class="sr-only">Password</label>
+			<input type="password" name="password" class="form-control" placeholder="Password" required="" autofocus="">
+
+			<div class="checkbox">
+		      <label>
+		        <input type="checkbox" value="remember-me"> Remember me
+		      </label>
+		    </div>
+
+			<button type="submit" name="submit" class="btn btn-lg btn-primary">Login</button>
+
+			<p>New to this site? <a href="<?php echo base_url('page/registration'); ?>">Register here!</a></p>
+
+		</form>
+	</div>
+</body>
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 
 <?php echo form_close(); ?>
--- application/views/templates/layouts/site/registermain.php
+++ application/views/templates/layouts/site/registermain.php
@@ -1,26 +1,73 @@
+<head>
+    {head}
+</head>
 <?php echo form_open(); ?>
-
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputFile">File input</label>
-    <input type="file" id="exampleInputFile">
-    <p class="help-block">Example block-level help text here.</p>
-  </div>
-  <div class="checkbox">
-    <label>
-      <input type="checkbox"> Check me out
-    </label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
+<body>
+	<div class="container">
+		<form class="form-signin">
+		  <h2 class="form-signin-heading">Register Here</h2>
+		  <div class="form-group">
+		    <label>Company Name: </label>
+		    <input type="text" class="form-control" id="companyname" placeholder="Company Name">
+		  </div>
+		  <div class="form-group">
+		    <label>Type of Company: </label>
+		    <input type="text" class="form-control" id="companytype" placeholder="Company Type">
+		  </div>
+		  <div class="form-group">
+		    <label>Address: </label>
+		    <input type="text" class="form-control" id="companyaddress" placeholder="Complete Address">
+		  </div>
+		  <div class="form-group">
+		    <label>Contact Number: </label>
+		    <input type="text" class="form-control" id="companycontact" placeholder="Contact Number">
+		  </div>
+		  <div class="form-group">
+		    <label>Email Address</label>
+		    <input type="email" class="form-control" id="companyemail" placeholder="(ex. company@gmail.com)">
+		  </div>
+		  <div class="form-group">
+		    <label>Owner / Founder: </label>
+		    <input type="text" class="form-control" id="companyfounder" placeholder="Complete Name">
+		  </div>
+		  <div class="form-group">
+		    <label>Year of Establishment: </label>
+		    <input type="text" class="form-control" id="companyyear" placeholder="(ex. 1990)">
+		  </div>
+		  <div class="form-group">
+		    <label>Human Resource Department's Contact Person: </label>
+		    <input type="text" class="form-control" id="hrname" placeholder="Complete Name">
+		  </div>
+		  <div class="form-group">
+		    <label>Department's Contact Number: </label>
+		    <input type="text" class="form-control" id="hrcontact" placeholder="HR Department's Contact Number">
+		  </div>
+		  <div class="form-group">
+		    <label>Department's E-mail Address: </label>
+		    <input type="email" class="form-control" id="hremail" placeholder="(ex. hrdepartment@gmail.com)">
+		  </div>
+		  <div class="form-group">
+		    <label>Job Position's in the company: </label>
+		    <input type="text" class="form-control" id="positions" placeholder="(ex.Encoder)">
+		  </div>
+		  <!-- <div class="form-group">
+		    <label for="exampleInputPassword1">Password</label>
+		    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputFile">File input</label>
+		    <input type="file" id="exampleInputFile">
+		    <p class="help-block">Example block-level help text here.</p>
+		  </div>
+		  <div class="checkbox">
+		    <label>
+		      <input type="checkbox"> Check me out
+		    </label>
+		  </div> -->
+		  <button type="submit" class="btn btn-lg btn-primary">Submit</button>
+		</form>
+	</div>
+</body>
 <!-- <div class="input-group">
   <span class="input-group-addon" id="basic-addon1">@</span>
   <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
