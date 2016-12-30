695dbd82be3fbeff1883c9238ead992976342ad1
Lemuel Francisco <lemuelrfrancisco@gmail.com>
2016-12-30 20:45:29 +0800

    modify register function

--- application/modules/page/controllers/Page.php
+++ application/modules/page/controllers/Page.php
@@ -18,13 +18,147 @@ var $validation = array(
         )
 	);
 
+var $company_validation = array(
+        array(
+                'field' => 'companyname',
+                'label' => 'Company Name',
+                'rules' => 'required'
+        ),
+        array(
+                'field' => 'companytype',
+                'label' => 'Company Type',
+                'rules' => 'required',
+                'errors' => array(
+                        'required' => 'You must provide a %s.',
+                )
+        ),
+        array(
+                'field' => 'companyaddress',
+                'label' => 'Company Address',
+                'rules' => 'required'
+        ),
+        array(
+                'field' => 'companycontact',
+                'label' => 'Company Contact',
+                'rules' => 'required'
+        ),
+        array(
+                'field' => 'companyemail',
+                'label' => 'Company Email',
+                'rules' => 'required'
+        ),
+        array(
+                'field' => 'companyfounder',
+                'label' => 'Company Founder',
+                'rules' => 'required'
+        ),
+        array(
+                'field' => 'companyyear',
+                'label' => 'Company Year',
+                'rules' => 'required'
+        ),
+        array(
+                'field' => 'hrname',
+                'label' => 'HR Name',
+                'rules' => 'required'
+        ),
+        array(
+                'field' => 'hrcontact',
+                'label' => 'HR Contact',
+                'rules' => 'required'
+        ),
+        array(
+                'field' => 'hremail',
+                'label' => 'HR Email',
+                'rules' => 'required'
+        ),
+        array(
+                'field' => 'positions',
+                'label' => 'Position',
+                'rules' => 'required'
+        )
+	);
+
+
 	public function index() {
 		$this->templates->layout('home');
 		$this->templates->render('home');
 	}
 	public function registration(){
-		$this->templates->layout('registermain');
-		$this->templates->render('registermain');
+		$this->templates->layout('default');
+	
+		$this->load->library('form_validation');
+			$this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
+		 $this->form_validation->set_rules($this->company_validation);
+			
+			foreach($this->company_validation as $row) {
+	            $this->data->$row['field']=NULL;
+	        }
+
+	 		
+            if ($this->input->post('submit') == NULL)
+            {
+            		/*
+            	die();
+					gumawa ako ng $this->data->fieldname tapos ipinasa ko sa view
+					ang mangyayari dyan, pag punta mo sa view, naka variable na agad yung 
+					field.
+					Example:
+					array(
+			                'field' => 'companyname',
+			                'label' => 'Company Name',
+			                'rules' => 'required'
+			        )
+
+			        pag punta mo sa view, naka ganito na yan, $companyname
+			        baka dyan ka nag unknown variable
+
+					*/
+            		
+            }
+            else {		
+            	//check ng code mo yung nasunod yung rules (form validation)
+            		$this->form_validation->run();
+
+            		foreach($this->company_validation as $row) {
+            			$this->data->$row['field']=$this->input->post($row['field']);
+            			$fields[]=$this->input->post($row['field']);
+            		}
+
+            		/* 
+					dito na ngayon malalagay yung pagtawag mo sa model
+            		example:
+            		$this->load->model("Page_model");
+        			$check=$this->Page_model->checklogin($username,$password);
+        			
+        			tapos maglalagay ka ng if() para icheck mo kung nagsave sa database
+
+        			parang ganto, 
+        			if($check) {
+						//redirect kung san page mo gusto
+        			} else {
+						echo "registration failed";
+        			}
+
+
+
+        			*/
+        			
+
+        			/*echo "<pre>";
+        			print_r($fields);
+        			echo "</pre>";
+        			die();
+        			*/
+
+        			//yung <pre> para lang maayos or magkaroon ng spacing yung pag print_r mo
+
+
+        			
+            }
+            print_r($this->data);
+		
+		$this->templates->render('registermain',$this->data);
 	}
 
 	public function login() {
--- application/modules/page/views/registermain.php
+++ application/modules/page/views/registermain.php
@@ -1,8 +1,64 @@
-<?php echo form_open(); ?>
-<div>
-	<input type="text" name="username">
-</div>
-<input type="password" name="password">
-<button type="submit" name="submit">Login</button>
-
+<?php echo form_open('','class="form-sign"'); ?>
+  <h2 class="form-signin-heading">Register Here</h2> 
+  	<div class="form-group">
+   	<?php echo form_label('Company Name','companyname'); ?>
+  	<?php echo form_error('companyname'); ?>
+    <?php echo form_input('companyname',$companyname,'class="form-control" placeholder="Company Name" id="companyname"'); ?>
+  </div>
+  <div class="form-group">
+    <?php echo form_label('Type of Company:','companytype'); ?>
+  	<?php echo form_error('companytype'); ?>
+    <?php echo form_input('companytype',$companytype,'class="form-control" placeholder="Type of Company:" id="companytype"'); ?>
+  </div>
+  <div class="form-group">
+    <label>Address: </label>
+    <input type="text" class="form-control" name="companyaddress" placeholder="Complete Address">
+  </div>
+  <div class="form-group">
+    <label>Contact Number: </label>
+    <input type="text" class="form-control" name="companycontact" placeholder="Contact Number">
+  </div>
+  <div class="form-group">
+    <label>Email Address</label>
+    <input type="email" class="form-control" name="companyemail" placeholder="(ex. company@gmail.com)">
+  </div>
+  <div class="form-group">
+    <label>Owner / Founder: </label>
+    <input type="text" class="form-control" name="companyfounder" placeholder="Complete Name">
+  </div>
+  <div class="form-group">
+    <label>Year of Establishment: </label>
+    <input type="text" class="form-control" name="companyyear" placeholder="(ex. 1990)">
+  </div>
+  <div class="form-group">
+    <label>Human Resource Department's Contact Person: </label>
+    <input type="text" class="form-control" name="hrname" placeholder="Complete Name">
+  </div>
+  <div class="form-group">
+    <label>Department's Contact Number: </label>
+    <input type="text" class="form-control" name="hrcontact" placeholder="HR Department's Contact Number">
+  </div>
+  <div class="form-group">
+    <label>Department's E-mail Address: </label>
+    <input type="email" class="form-control" name="hremail" placeholder="(ex. hrdepartment@gmail.com)">
+  </div>
+  <div class="form-group">
+    <label>Job Position's in the company: </label>
+    <input type="text" class="form-control" name="positions" placeholder="(ex.Encoder)">
+  </div>
+  <!-- <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+    <input type="file" id="exampleInputFile">
+    <p class="help-block">Example block-level help text here.</p>
+  </div>
+  <div class="checkbox">
+    <label>
+      <input type="checkbox"> Check me out
+    </label>
+  </div> -->
+  <button type="submit" name="submit" class="btn btn-lg btn-primary" value="1">Submit</button>
 <?php echo form_close(); ?>
