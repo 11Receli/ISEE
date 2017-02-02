f3dc8c833f8bfda6d3d4fb19d8ef4e2125ede59d
lexpogi1 <fatimareginabenitez@gmail.com>
2017-02-02 15:38:43 +0800

    Add files via upload

---
+++ application/views/templates/layouts/site/profileUpdateView.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    {head}
+</head>
+<body>
+    <input type="hidden" id="base-url" value="<?php echo base_url(); ?>">
+    {content}
+    {scripts}
+</body>
+
+</html>
